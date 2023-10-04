87f21fda36fbc1b4f8abe10d194143758daef59a
aarthi <aarthinb8@gmail.com>
2023-10-04 09:44:06 +0530

    modified app.js and ourdomains

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Aboutus from './components/aboutus';
+import OurDomains from '/.components/ourdomains';
 import Events from './components/events';
 import Landing_page from './components/landing-page';
 import Index from './components/testimonials';
@@ -11,6 +12,7 @@ function App() {
          <Landing_page/>
          <Aboutus/>
          <Events/>
+         <OurDomains/>
          <Gallery/>
          <Index/>
     </div>
